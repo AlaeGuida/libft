189f024fb0751ae1a2ae1b57d0181c50ae5cec08
Alae Eddine Guida <aguida@c2r8p6.1337.ma>
2022-10-13 16:42:39 +0100

    libft_backup

---
+++ libft/ft_strjoin.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aguida <aguida@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/13 15:12:46 by aguida            #+#    #+#             */
+/*   Updated: 2022/10/13 16:38:04 by aguida           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*str;
+	size_t	i;
+	size_t	n;
+
+	i = 0;
+	n = 0;
+	str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2)
+				+ 1));
+	if (!str)
+		return (0);
+	while (s1[i] != '\0')
+	{
+		str[i] = s1[n];
+		i++;
+		n++;
+	}
+	n = 0;
+	while (s2[n] != '\0')
+	{
+		str[i] = s2[n];
+		i++;
+		n++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+/*
+#include <stdio.h>
+int	main(void)
+{
+    char str[] = "hello ";
+    char str2[] = "1337";
+
+    printf("%s\n", ft_strjoin(str, str2));
+}*/
---
+++ libft/ft_substr.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aguida <aguida@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/13 11:42:02 by aguida            #+#    #+#             */
+/*   Updated: 2022/10/13 13:11:05 by aguida           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	size_t	i;
+	size_t	s_len;
+	char	*str;
+
+	s_len = ft_strlen(s);
+	i = 0;
+	str = (char *)malloc(sizeof(char) * (len + 1));
+	if (!str)
+		return (0);
+	while (i < len && start < s_len && s[start] != '\0')
+	{
+		str[i] = s[start];
+		start++;
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+/*
+#include <stdio.h>
+int	main(void)
+{
+    char str[] = "hello 1337";
+    int start = 12;
+    int length = 10;
+    
+    char *result = ft_substr(str, start, length);
+    printf("%s\n", result);
+}*/
